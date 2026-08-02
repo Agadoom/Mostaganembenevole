eaed4343ee8857f841744409fb525910866e40a6
DeepTrade <141333737+Agadoom@users.noreply.github.com>
2026-08-02 17:18:38 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'registre-familly.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
